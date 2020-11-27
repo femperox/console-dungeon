70b0fa481ce9fb09d5b633227d639c03e91187e6
manCREEPman <barbekyan@yandex.ru>
2020-11-27 18:23:37 +0300

    Заккоментированы основные изменения в третьей ветке: файл rooms, изменения в файле commands

--- src/mire/commands.clj
+++ src/mire/commands.clj
@@ -4,17 +4,24 @@
 
 ;; Command functions
 
+;; Команда look, не принимает параметров, она получает как строчку описание комнаты,
+;; затем показывает, в какую комнату идёт выход
 (defn look "Get a description of the surrounding environs and its contents."
   []
   (str (:desc *current-room*)
        "\nExits: " (keys (:exits *current-room*))
        ".\n"))
 
+;; Команда move принимает параметр direction - строка направления, например north
 (defn move
   "\"♬ We gotta get out of this place... ♪\" Give a direction."
   [direction]
+  ;; здесь target-name получает ключевое имя комнаты
+  ;; а target будет соответствовать hash-map-у комнаты, куда мы попали, если комната в этом направлении существует
   (let [target-name ((:exits *current-room*) (keyword direction))
         target (rooms target-name)]
+    ;; Если комната существует, то вызывается функция, устанавливающая текущую комнату, после вызывается look,
+    ;; иначе выводится надпись "You can't go that way."
     (if target
       (do (set-current-room target)
           (look))
@@ -22,11 +29,12 @@
 
 ;; Command data
 
-(def commands {"move" move,
-               "north" (fn [] (move :north)),
-               "south" (fn [] (move :south)),
-               "east" (fn [] (move :east)),
-               "west" (fn [] (move :west)),
+;; Объявляются ассоциации строчек и функций, которым они соответствуют
+(def commands {"move" move
+               "north" (fn [] (move :north))
+               "south" (fn [] (move :south))
+               "east" (fn [] (move :east))
+               "west" (fn [] (move :west))
                "look" look})
 
 ;; Command handling
--- src/mire/rooms.clj
+++ src/mire/rooms.clj
@@ -1,13 +1,17 @@
+;; Прописали комнаты в пространство имён
 (ns mire.rooms
   (:use [clojure.contrib str-utils]))
 
+;; Определяем комнаты
+;; rooms - hash-map, в котором 2 комнаты - start, closet, которые также являются hash-map-ами
 (def rooms
      {:start {:desc "You find yourself in a round room with a pillar in the middle."
               :exits {:north :closet}}
       :closet {:desc "You are in a cramped closet."
                 :exits {:south :start}}})
-
+;; создаём глобальную переменную текущей комнаты, передавая hash-map стартовой комнаты
 (def *current-room* (rooms :start))
 
+;; Определяем функцию, 
 (defn set-current-room [target]
      (def *current-room* target))
