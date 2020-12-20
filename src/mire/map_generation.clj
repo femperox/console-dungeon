ecf6cc6dce7ae35497d6dff0ffdd7dcba33e17fd
manCREEPman <barbekyan@yandex.ru>
2020-12-20 18:39:40 +0300

    Добавлена функция, возвращающая сет из n случайных элементов другого сета

--- src/mire/map_generation.clj
+++ src/mire/map_generation.clj
@@ -49,5 +49,60 @@
   )
 )
 
-(gen-lvl rooms_graf_head 5) ; создание двунаправленного списка 
-(through rooms_graf_head) ; просмотр в обе стороны
+
+(defn pick-rand
+  [vect]
+  (vect (rand-int (count vect))))
+
+
+(defn get-n-rand-from-set [n set]
+  (loop [iter n
+        rand-n-set set] 
+    (let [rand-value (rand-int (count rand-n-set))]
+      (if (> iter 0)
+        (recur
+          (- iter 1)
+          (disj rand-n-set (nth (vec rand-n-set) rand-value))
+        )
+        rand-n-set
+      )
+    )
+  )
+)
+
+(defn gen-sides [origin-way]
+  (let [sides (disj #{:north :south :west :east} origin-way)
+        sides-count (randint 3)]
+    (if (= sides-count 2)
+      sides
+      (loop []
+
+      )
+    )
+  )
+)
+
+; принимает ключевое слово направления и возвращает противоположное
+(defn opposite_way [way]
+  (keyword (way {:north "south" :east "west" :south "north" :west "east"}))  
+)
+
+(defn gen-head [graf_head]
+  (dosync
+    (commute graf_head assoc :exits (ref {}))
+    (doseq [world_side [:north :east :south :west]] 
+      (commute (:exits @graf_head) assoc world_side (ref {}))
+      (commute (world_side @(:exits @graf_head)) assoc (opposite_way world_side) graf_head)
+    )
+  )
+)
+
+; (gen-lvl rooms_graf_head 5) ; создание двунаправленного списка 
+; (through rooms_graf_head) ; просмотр в обе стороны
+(gen-head rooms_graf_head)
+(print rooms_graf_head)
+
+; (defn pr [subj] (println subj))
+
+; (println (for [w '(:north :east :south :west)] w)) ; :north :east :south :west "north" "east" "south" "west"
+; (doseq [w [:north :east :south :west]] (pr w))
