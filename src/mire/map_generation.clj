59438987047054bb9f097979b00022c01a7cabff
manCREEPman <barbekyan@yandex.ru>
2020-12-20 04:24:56 +0300

    Третья потуга

--- src/mire/map_generation.clj
+++ src/mire/map_generation.clj
@@ -3,22 +3,54 @@
 (def room_struct (ref {}))
 
 (defn gen-lvl [cur-room lvl]
-  (dosync 
-   (loop [lv lvl
-          cr cur-room]
-         (do
-           (commute cr assoc :room-name (apply str ["room " lv]) :exits (ref {}))
-           (commute (:exits cr) assoc  :next (ref {}))
-           (if (not= lv 1)
-             (do
-               (commute (:exits cr) assoc :next (ref {}))
-               (commute (:next (:exits cr)) assoc :prev cr))
-            )
-           (if (< lv 5)
-             (recur (:next (:exits cr)) (+ lv 1)))
-            )
-           )
-         )
-    )
+  (try
+  (dosync
+    (loop [lv 1
+          cr cur-room
+          pr nil]
+          
+          ;  (println cr)
+          ;  (println lv)
+            (do 
 
+                (commute cr assoc :room-name (apply str ["room " lv])) 
+                (commute cr assoc :exits (ref {})) 
+                (commute (:exits @cr) assoc :next (ref {}))
+                (commute (:exits @cr) assoc :prev pr)
+                (println lv)
+                (println cr)
+              ; (if (= lv 1)
+              ;   ; (do
+              ;   ;   (commute (:exits @cr) assoc :next (ref {}))
+              ;   ;   (commute (:next @(:exits @cr)) assoc :prev cr))
+              ;   (commute (:exits @cr) assoc :prev nil)
+              ; )
+                  (if (< lv lvl)
+                    (do
+                      (println "we are here")
+                      (recur (+ lv 1) (:next @(:exits @cr)) (cr))
+                    )
+                    (commute (:exits @cr) assoc :next nil)
+                  )
+            )
+    )
+  )
+    (catch Exception e
+    (.printStackTrace e (new java.io.PrintWriter *err*))) 
+  )
+)
+
+(defn through [room]
+  (loop [r room]
+    (do
+      (println (:room-name @r))
+      (if (not= (:next @(:exits @r)) nil)
+        (recur (:next @(:exits @r)))
+      )
+    )
+  )
+)
+
+(gen-lvl room_struct 3)
+; (through room_struct)
 ;; assoc для добавления новой пары ключ-значение
