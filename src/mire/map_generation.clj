26ee2679a087692d936a74fb4edf9a8bf40d5438
manCREEPman <barbekyan@yandex.ru>
2020-12-20 10:50:05 +0300

    Рефакторинг примера

--- src/mire/map_generation.clj
+++ src/mire/map_generation.clj
@@ -1,28 +1,26 @@
 (ns map_generation)
 
-(def room_struct (ref {}))
+(def rooms_graf_head (ref {}))
 
 (defn gen-lvl [cur-room lvl]
   (try
   (dosync
-    (loop [lv 1
-          cr cur-room
-          pr nil]
-            (do 
-
-                (commute cr assoc :room-name (apply str ["room " lv])) 
-                (commute cr assoc :exits (ref {})) 
-                (commute (:exits @cr) assoc :next (ref {}))
-                (commute (:exits @cr) assoc :prev pr)
-                (println lv)
-                  (if (< lv lvl)
-                    (do
-                      (println "we are here")
-                      (recur (+ lv 1) (:next @(:exits @cr)) cr)
-                    )
-                    (commute (:exits @cr) assoc :next nil)
-                  )
+    (loop 
+      [lv 1
+      cr cur-room
+      pr nil]
+      (do 
+        (commute cr assoc :room-name (apply str ["room " lv]) :exits (ref {})) 
+        (commute (:exits @cr) assoc :next (ref {}) :prev pr)
+        (println lv)
+          (if (< lv lvl)
+            (do
+              (println "we are here")
+              (recur (+ lv 1) (:next @(:exits @cr)) cr)
             )
+            (commute (:exits @cr) assoc :next nil)
+          )
+      )
     )
   )
     (catch Exception e
@@ -51,5 +49,5 @@
   )
 )
 
-(gen-lvl room_struct 5) ; создание двунаправленного списка 
-(through room_struct) ; просмотр в одну сторону
+(gen-lvl rooms_graf_head 5) ; создание двунаправленного списка 
+(through rooms_graf_head) ; просмотр в обе стороны
