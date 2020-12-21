ccf49921c579f285c6358a71e19ad9b47e24d277
manCREEPman <barbekyan@yandex.ru>
2020-12-21 21:03:38 +0300

    Описаны функции и тесты создания графа карты игры

--- src/mire/map_generation.clj
+++ src/mire/map_generation.clj
@@ -2,15 +2,16 @@
 
 (def rooms_graf_head (ref {}))
 
-(defn gen-lvl [cur-room lvl]
+(defn gen_lvl [cur_room lvl]
+  "Создание комнаты на lvl-ном уровне графа"
   (try
   (dosync
     (loop 
       [lv 1
-      cr cur-room
+      cr cur_room
       pr nil]
       (do 
-        (commute cr assoc :room-name (apply str ["room " lv]) :exits (ref {})) 
+        (commute cr assoc :room_name (apply str ["room " lv]) :exits (ref {})) 
         (commute (:exits @cr) assoc :next (ref {}) :prev pr)
         (println lv)
           (if (< lv lvl)
@@ -28,77 +29,93 @@
   )
 )
 
-(defn through [room]
-  (loop [r room]
-    (do
-      (println (:room-name @r))
-      (if (not= (:next @(:exits @r)) nil)
-        (recur (:next @(:exits @r)))
-        (def final r)
-      )
-    )
-  )
-  (println)
-  (loop [r final]
-    (do
-      (println (:room-name @r))
-      (if (not= (:prev @(:exits @r))) nil
-        (recur (:prev @(:exits @r)))
-      )
-    )
-  )
-)
-
-(defn get-n-rand-from-set [n set]
+(defn get_n_rand_from_set [n set]
+  "Возвращает сет из n случайных элементов переданного сета (n <= len(set))"
   (loop [iter n
-        rand-n-set #{}
-        old-set set] 
-    (let [rand-value (rand-int (count old-set))]
+        rand_n_set #{}
+        old_set set] 
+    (let [rand_value (rand-int (count old_set))]
       (if (> iter 0)
         (recur
           (- iter 1)
-          (conj rand-n-set (nth (vec old-set) rand-value))
-          (disj old-set (nth (vec old-set) rand-value))
+          (conj rand_n_set (nth (vec old_set) rand_value))
+          (disj old_set (nth (vec old_set) rand_value))
         )
-        rand-n-set
+        rand_n_set
       )
     )
   )
 )
 
-(defn gen-sides [origin-way]
-  (let [sides (disj #{:north :south :west :east} origin-way)
-        sides-count (randint 3)]
-    (if (= sides-count 2)
-      sides
-      (loop []
-
-      )
-    )
+(defn gen_sides [origin_way]
+  "Принимает ключ, возвращает случайный сет размерности от 0 до 3,
+   состоящий из случайных ключей сторон света"
+  (let [sides (disj #{:north :south :west :east} origin_way)
+        sides_count (if (nil? origin_way) 4 (rand-int 4))]
+    (get_n_rand_from_set sides_count sides)
   )
 )
 
-; принимает ключевое слово направления и возвращает противоположное
 (defn opposite_way [way]
+  "Возвращает ключ противоположной стороны света"
   (keyword (way {:north "south" :east "west" :south "north" :west "east"}))  
 )
 
-(defn gen-head [graf_head]
+(defn gen_graph [current_room direction_from_arrived prev_room lvl]
+  "Принимает ссылку на мапу; 
+   сторону света, откуда пришли в комнату;
+   ссылку на мапу, откуда пришли в комнату;
+   уровень графа.
+   Создаёт граф комнат. 
+   Переданная вначале ссылка будет указателем на структуру"
   (dosync
-    (commute graf_head assoc :exits (ref {}))
-    (doseq [world_side [:north :east :south :west]] 
-      (commute (:exits @graf_head) assoc world_side (ref {}))
-      (commute (world_side @(:exits @graf_head)) assoc (opposite_way world_side) graf_head)
+    (commute current_room assoc :exits (ref {}))
+    (commute current_room assoc :name (str "room " lvl "-" (rand-int 1000)))
+    (if (not (nil? direction_from_arrived))
+      (commute (:exits @current_room) assoc (opposite_way direction_from_arrived) prev_room)
+    )
+    (if (> (- lvl 1) 0) 
+       (doseq [direction (gen_sides direction_from_arrived)]
+        (commute (:exits @current_room) assoc direction (ref {}))
+        (gen_graph 
+          (direction @(:exits @current_room))
+          direction
+          current_room
+          (- lvl 1)
+        )
+      ) 
     )
   )
 )
 
-; (gen-lvl rooms_graf_head 5) ; создание двунаправленного списка 
-; (through rooms_graf_head) ; просмотр в обе стороны
-(gen-head rooms_graf_head)
-(print rooms_graf_head)
+(defn print_room [room name_from]
+  "Тестовая функция. Выводит название комнаты и
+   название комнаты, откуда мы пришли"
+  (println (str (:name @room) " from " name_from))
+  (print "exits: ")
+  (doseq [dirs (keys @(:exits @room))] (print dirs " "))
+  (println "")
+)
 
-; (defn pr [subj] (println subj))
+(defn see_graph [graph dir_from name_from]
+  "Просмотр графа.
+   Принимает ссылку на мапу комнаты;
+   направление, откуда пришли в комнату
+   название комнаты, откуда пришли"
+  (if (> (count (keys @(:exits @graph))) 1)
+    (do
+      (print_room graph name_from)
+      (doseq [dirs (keys @(:exits @graph))]
+        (if (not (= dirs dir_from))
+          (see_graph (dirs @(:exits @graph)) (opposite_way dirs) (:name @graph))
+        )
+      )
+    )
+    (print_room graph name_from)
+  )
+)
 
-; (println (for [w '(:north :east :south :west)] w)) ; :north :east :south :west "north" "east" "south" "west"
-; (doseq [w [:north :east :south :west]] (pr w))
+; Тест
+; создаём граф и выводим его на экран
+(gen_graph rooms_graf_head nil nil 4)
+(see_graph rooms_graf_head nil nil)
